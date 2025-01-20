1aef4b9821eadbb2bf223db6fdd2a03435a38f2e
hFahadDev <hfahad534@gmail.com>
2025-01-21 02:15:38 +0500

    Removed sensitive firebase credentials

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDt7Bpz--QqercXhj83UtpW7V_f5wNZiJ8",
-  authDomain: "hotel-management-83b73.firebaseapp.com",
-  projectId: "hotel-management-83b73",
-  storageBucket: "hotel-management-83b73.appspot.com",
-  messagingSenderId: "934600684549",
-  appId: "1:934600684549:web:0be0088030b25aecff986c",
-  measurementId: "G-P23N844P2H"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
